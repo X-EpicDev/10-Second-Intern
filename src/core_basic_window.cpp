db5d457beb56ec0447054281c9399472575395dc
doom <61824552+JustDoom@users.noreply.github.com>
2024-11-29 17:37:54 +1000

    git gud

--- src/core_basic_window.cpp
+++ src/core_basic_window.cpp
@@ -4,11 +4,7 @@
 #define MAX(a, b) ((a)>(b)? (a) : (b))
 #define MIN(a, b) ((a)<(b)? (a) : (b))
 
-//------------------------------------------------------------------------------------
-// Program main entry point
-//------------------------------------------------------------------------------------
-int main(void)
-{
+int main(void) {
     //
     const int windowWidth = 1080;
     const int windowHeight = 720;
@@ -18,25 +14,20 @@ int main(void)
     InitWindow(windowWidth, windowHeight, "Resize Window");
     SetWindowMinSize(320, 240);
 
-    // current game size
     int gameScreenWidth = 1080;
     int gameScreenHeight = 720;
 
-    // Render texture initialization, used to hold the rendering result so we can easily resize it
     RenderTexture2D target = LoadRenderTexture(gameScreenWidth, gameScreenHeight);
-    SetTextureFilter(target.texture, TEXTURE_FILTER_BILINEAR);  // Texture scale filter to use
+    SetTextureFilter(target.texture, TEXTURE_FILTER_BILINEAR);
 
-    Texture2D sheet =  LoadTexture("../sheet.png");
-    Rectangle playerDestRect = { 100, 100, 64, 64 };
+    Texture2D sheet = LoadTexture("../sheet.png");
+    Rectangle playerDestRect = {100, 100, 64, 64};
 
-    SetTargetFPS(60);                   // Set our game to run at 60 frames-per-second
+    SetTargetFPS(60);
 
-    // Main game loop
-    while (!WindowShouldClose())        // Detect window close button or ESC key
+    while (!WindowShouldClose())
     {
         // Update
-        //----------------------------------------------------------------------------------
-        // Compute the size
         float scale = MIN((float)GetScreenWidth()/gameScreenWidth, (float)GetScreenHeight()/gameScreenHeight);
 
         if (IsKeyDown('D')) {
@@ -47,28 +38,25 @@ int main(void)
 
         // Update virtual mouse (clamped mouse value behind game screen)
         Vector2 mouse = GetMousePosition();
-        Vector2 virtualMouse = { 0 };
-        virtualMouse.x = (mouse.x - (GetScreenWidth() - (gameScreenWidth*scale))*0.5f)/scale;
-        virtualMouse.y = (mouse.y - (GetScreenHeight() - (gameScreenHeight*scale))*0.5f)/scale;
-        virtualMouse = Vector2Clamp(virtualMouse, (Vector2){ 0, 0 }, (Vector2){ (float)gameScreenWidth, (float)gameScreenHeight });
+        Vector2 virtualMouse = {0};
+        virtualMouse.x = (mouse.x - (GetScreenWidth() - (gameScreenWidth * scale)) * 0.5f) / scale;
+        virtualMouse.y = (mouse.y - (GetScreenHeight() - (gameScreenHeight * scale)) * 0.5f) / scale;
+        virtualMouse = Vector2Clamp(virtualMouse, (Vector2){0, 0},
+                                    (Vector2){(float) gameScreenWidth, (float) gameScreenHeight});
 
         //drawing
         BeginDrawing();
-        ClearBackground(BLACK);     // Clear screen background
+        ClearBackground(BLACK); // Clear screen background
 
         // Circle Drawing
-        DrawCircle((GetScreenWidth() - ((float)100*scale))*0.5f, (GetScreenHeight() - ((float)100*scale))*0.5f, 100, RED);
-        DrawCircle(GetScreenWidth()/2, GetScreenHeight()/2, 100, BLUE);
-        DrawTexturePro(sheet, { 16, 0, 16, 16 }, playerDestRect, { 0, 0 }, 0.0f, WHITE);
+        DrawCircle((GetScreenWidth() - ((float) 100 * scale)) * 0.5f,(GetScreenHeight() - ((float) 100 * scale)) * 0.5f, 100, RED);
+        DrawCircle(GetScreenWidth() / 2, GetScreenHeight() / 2, 100, BLUE);
+        DrawTexturePro(sheet, {16, 0, 16, 16}, playerDestRect, {0, 0}, 0.0f, WHITE);
         EndDrawing();
-        //--------------------------------------------------------------------------------------
     }
 
-    // De-Initialization
-    //--------------------------------------------------------------------------------------
-    UnloadRenderTexture(target);        // Unload render texture
-    CloseWindow();                      // Close window and OpenGL context
-    //--------------------------------------------------------------------------------------
+    UnloadRenderTexture(target);
+    CloseWindow();
 
     return 0;
 }
