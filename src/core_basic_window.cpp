96d8903b0fd85ecc7ea077cb6bb76aeba3d73644
doom <61824552+JustDoom@users.noreply.github.com>
2024-11-29 17:56:25 +1000

    Fix centring camera on player when resizing window

--- src/core_basic_window.cpp
+++ src/core_basic_window.cpp
@@ -47,6 +47,7 @@ int main(void) {
             playerDestRect.y -= movementSpeed * deltaTime;
         }
 
+        camera.offset = (Vector2){GetScreenWidth() / 2.0f, GetScreenHeight() / 2.0f};
         camera.target = (Vector2){playerDestRect.x + playerDestRect.width / 2, playerDestRect.y + playerDestRect.height / 2};
 
         Vector2 mouse = GetMousePosition();
@@ -60,7 +61,7 @@ int main(void) {
 
         DrawCircle(400, 300, 50, RED);
         DrawCircle(600, 500, 100, BLUE);
-        DrawTexturePro(sheet, (Rectangle){16, 0, 16, 16}, playerDestRect, (Vector2){0, 0}, 0.0f, WHITE); // Player texture
+        DrawTexturePro(sheet, (Rectangle){16, 0, 16, 16}, playerDestRect, (Vector2){0, 0}, 0.0f, WHITE);
 
         EndMode2D();
 
