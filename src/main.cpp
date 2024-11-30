d2b26100a89b3a5c030575a4a7a7883adda5d415
doom <61824552+JustDoom@users.noreply.github.com>
2024-11-30 17:07:10 +1000

    Add timer

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,26 @@
+#include <cmath>
+#include <iomanip>
+#include <sstream>
 #include <string>
 
 #include "Object.h"
 #include "raylib.h"
+
+enum GameState {
+    WAITING,
+    PLAYING,
+    FINISHED
+};
 
 int main() {
     const int windowWidth = 1080;
     const int windowHeight = 720;
     const float movementSpeed = 75.0f;
     bool debug = false;
+
+    GameState gameState = WAITING;
+    float timer = 10;
+    int score = 0;
 
     // Enable config flags for resizable window and vertical synchro
     SetConfigFlags(FLAG_WINDOW_RESIZABLE);
@@ -23,8 +36,16 @@ int main() {
     Texture2D sheet = LoadTexture("../assets/sheet.png");
     Texture2D mapText = LoadTexture("../assets/office.png");
 
+    // Text
+    std::string startText = "Press SPACE or ENTER to begin";
+    int startTextWidth = MeasureText(startText.c_str(), 20);
+
+    std::string gameOverText = "Game Over";
+    int gameOverTextWidth = MeasureText(gameOverText.c_str(), 40);
+
+    // Objects
     Object player(sheet, {16.0001, 0, 15.9999, 15.9999}, {25, 25, 16, 16}, {0, 0, 8, 15}, Vector2{4, 1});
-    Object map(mapText, {0,0,448,320}, {0,0,448,320}, {0,0,0,0}, Vector2{0, 0});
+    Object map(mapText, {0, 0, 448, 320}, {0, 0, 448, 320}, {0, 0, 0, 0}, Vector2{0, 0});
 
     Camera2D camera;
     camera.rotation = 0.0f;
@@ -34,20 +55,44 @@ int main() {
         const float deltaTime = GetFrameTime();
 
         // Update
-        if (IsKeyDown(KEY_D)) {
-            player.setX(player.getX() + movementSpeed * deltaTime);
-        }
-        if (IsKeyDown(KEY_A)) {
-            player.setX(player.getX() - movementSpeed * deltaTime);
-        }
-        if (IsKeyDown(KEY_S)) {
-            player.setY(player.getY() + movementSpeed * deltaTime);
-        }
-        if (IsKeyDown(KEY_W)) {
-            player.setY(player.getY() - movementSpeed * deltaTime);
-        }
-        if (IsKeyPressed(KEY_F3)) {
-            debug = !debug;
+        switch (gameState) {
+            case WAITING:
+                if (IsKeyPressed(KEY_ENTER) || IsKeyPressed(KEY_SPACE)) {
+                    gameState = PLAYING;
+                }
+
+                break;
+            case PLAYING:
+                if (timer <= 0) {
+                    gameState = FINISHED;
+                }
+
+                if (IsKeyDown(KEY_D)) {
+                    player.setX(player.getX() + movementSpeed * deltaTime);
+                }
+                if (IsKeyDown(KEY_A)) {
+                    player.setX(player.getX() - movementSpeed * deltaTime);
+                }
+                if (IsKeyDown(KEY_S)) {
+                    player.setY(player.getY() + movementSpeed * deltaTime);
+                }
+                if (IsKeyDown(KEY_W)) {
+                    player.setY(player.getY() - movementSpeed * deltaTime);
+                }
+                if (IsKeyPressed(KEY_F3)) {
+                    debug = !debug;
+                }
+
+                timer -= deltaTime;
+
+                break;
+            case FINISHED:
+                if (IsKeyPressed(KEY_ENTER) || IsKeyPressed(KEY_SPACE)) {
+                    // reset();
+                    gameState = PLAYING;
+                }
+
+                break;
         }
 
         camera.offset = (Vector2){GetScreenWidth() / 2.0f, GetScreenHeight() / 2.0f};
@@ -59,29 +104,45 @@ int main() {
         // Drawing
         BeginDrawing();
         ClearBackground(BLACK);
-
         BeginMode2D(camera);
 
         map.draw();
 
         DrawCircle(100, 75, 12.5, RED);
-        if(CheckCollisionCircleRec((Vector2){100,75}, 12.5, player.getHitbox())) {
-            DrawText("Skibidi", 25,25,5, WHITE);
+        if (CheckCollisionCircleRec((Vector2){100, 75}, 12.5, player.getHitbox())) {
+            DrawText("Skibidi", 25, 25, 5, WHITE);
         }
 
         player.draw();
 
         EndMode2D();
 
+        std::ostringstream stream;
+        switch (gameState) {
+            case WAITING:
+                DrawText(startText.c_str(), GetScreenWidth() / 2 - startTextWidth / 2, GetScreenHeight() / 5 * 3.5f, 20, WHITE);
+                break;
+            case FINISHED:
+                DrawText(gameOverText.c_str(), GetScreenWidth() / 2 - gameOverTextWidth / 2, GetScreenHeight() / 3, 40, WHITE);
+                // No break so it still renders score/timer for now
+            case PLAYING:
+                DrawText(("Score: " + std::to_string(score)).c_str(), 0, 0, 20, WHITE);
+                // Format to 1 decimal place
+                stream << std::fixed << std::setprecision(1) << std::round(timer * 10) / 10.0;
+                DrawText(("Time: " + stream.str()).c_str(), 0, 16, 20, WHITE);
+                break;
+        }
+
         // Debug
         if (debug) {
-            DrawFPS(0, 0);
-            DrawText(("Position: " + std::to_string(player.getX()) + "," + std::to_string(player.getY())).c_str(), 0, 16, 20, WHITE);
-            DrawText(("DeltaTime: " + std::to_string(deltaTime)).c_str(), 0, 32, 20, WHITE);
+            DrawFPS(0, 32);
+            DrawText(("Position: " + std::to_string(player.getX()) + "," + std::to_string(player.getY())).c_str(), 0,
+                     48, 20, WHITE);
+            DrawText(("DeltaTime: " + std::to_string(deltaTime)).c_str(), 0, 64, 20, WHITE);
         }
+
         EndDrawing();
     }
-
 
     UnloadRenderTexture(target);
     CloseWindow();
