46ab34e0fd3dabf956cc8c450754338a208f86ab
doom <61824552+JustDoom@users.noreply.github.com>
2024-12-02 09:21:11 +1000

    Get wall collisions working at the cost of uncapped FPS

--- src/main.cpp
+++ src/main.cpp
@@ -12,6 +12,19 @@ enum GameState {
     FINISHED
 };
 
+bool CheckCollisionWithTexture(const Rectangle rect, const Color* pixels, const int textureWidth, const int textureHeight) {
+    for (int y = static_cast<int>(rect.y); y < rect.y + rect.height; y++) {
+        for (int x = static_cast<int>(rect.x); x < rect.x + rect.width; x++) {
+            if (x >= 0 && x < textureWidth && y >= 0 && y < textureHeight) {
+                if (pixels[y * textureWidth + x].a > 0) {
+                    return true;
+                }
+            }
+        }
+    }
+    return false;
+}
+
 int main() {
     const int windowWidth = 1080;
     const int windowHeight = 720;
@@ -19,7 +32,7 @@ int main() {
     bool debug = false;
 
     GameState gameState = WAITING;
-    float timer = 10;
+    float timer = 100;
     int score = 0;
 
     // Enable config flags for resizable window and vertical synchro
@@ -27,14 +40,15 @@ int main() {
     InitWindow(windowWidth, windowHeight, "Resize Window");
     SetWindowMinSize(320, 240);
 
-    SetTargetFPS(60);
+    // SetTargetFPS(60);
 
     RenderTexture2D target = LoadRenderTexture(windowWidth, windowHeight);
     SetTextureFilter(target.texture, TEXTURE_FILTER_BILINEAR);
 
     // Load textures
     Texture2D sheet = LoadTexture("../assets/sheet.png");
-    Texture2D mapText = LoadTexture("../assets/office.png");
+    Texture2D officeWalls = LoadTexture("../assets/office_walls.png");
+    Texture2D officeFloor = LoadTexture("../assets/office_floor.png");
 
     // Text
     std::string startText = "Press SPACE or ENTER to begin";
@@ -45,59 +59,10 @@ int main() {
 
     // Objects
     Object player(sheet, {16.0001, 0, 15.9999, 15.9999}, {25, 25, 16, 16}, {0, 0, 8, 7}, Vector2{4, 9});
-    Object map(mapText, {0, 0, 448, 320}, {0, 0, 448, 320}, {0, 0, 0, 0}, Vector2{0, 0});
-
-    //top hitboxes
-    Rectangle t1 = {16,15, 256, 1};
-    Rectangle t2 = {288,15, 80, 1};
-    Rectangle t3 = {176,111, 111, 1};
-    Rectangle t4 = {16,159, 63, 1};
-    Rectangle t5 = {97,159, 78, 1};
-    Rectangle t6 = {273,159, 63, 1};
-    Rectangle t7 = {352,159, 80, 1};
-    Rectangle t8 = {273,239, 14, 1};
-    Rectangle t9 = {16,223, 95, 1};
-    Rectangle t10 = {129,223, 79, 1};
-    Rectangle t11 = {288,223, 48, 1};
-    Rectangle t12 = {273,239, 14, 1};
-    Rectangle t13 = {337, 175, 14, 1};
-
-    //bottom hitboxes
-    Rectangle b1 = {161,104, 111, 1};
-    Rectangle b2 = {16,152, 63, 1};
-    Rectangle b3 = {97,152, 63, 1};
-    Rectangle b4 = {273,152, 95, 1};
-    Rectangle b5 = {337,200, 14, 1};
-    Rectangle b6 = {16,216, 95, 1};
-    Rectangle b7 = {129,216, 94, 1};
-    Rectangle b8 = {273,216, 63, 1};
-    Rectangle b9 = {273, 264, 14, 1};
-    Rectangle b10 = {16, 312, 416, 1};
-    //left hitboxes
-    Rectangle l1 = {272,15,1,90};
-    Rectangle l2 = {368,15,1,138};
-    Rectangle l3 = {160,104,1,49};
-    Rectangle l4 = {96,152,1,8};
-    Rectangle l5 = {272,152,1,8};
-    Rectangle l6 = {336,159,1,17};
-    Rectangle l7 = {336,200,1,112};
-    Rectangle l8 = {128,216,1,8};
-    Rectangle l9 = {272,216,1,24};
-    Rectangle l10 = {208,223,1,89};
-    Rectangle l11 = {272,264,1,48};
-    Rectangle l12 = {432, 159, 154, 1};
-
-    //right hitboxes
-    Rectangle r1 = {15,15,1,298};
-    Rectangle r2 = {287,15,1,97};
-    Rectangle r3 = {175,111,1,49};
-    Rectangle r4 = {79,152,1,8};
-    Rectangle r5 = {351,200,1,112};
-    Rectangle r6 = {111,216,1,8};
-    Rectangle r7 = {223,216,1,96};
-    Rectangle r8 = {287,223,1,17};
-    Rectangle r9 = {287,264,1,48};
-    Rectangle r10 = {351,159,1,17};
+    Object walls(officeWalls, {0, 0, 448, 320}, {0, 0, 448, 320}, {0, 0, 0, 0}, Vector2{0, 0});
+    Object floor(officeFloor, {0, 0, 448, 320}, {0, 0, 448, 320}, {0, 0, 0, 0}, Vector2{0, 0});
+    Image officeWallsImage = LoadImage("../assets/office_walls.png");
+    Color* wallPixels = LoadImageColors(officeWallsImage);
 
     Camera2D camera;
     camera.rotation = 0.0f;
@@ -111,6 +76,10 @@ int main() {
         }
 
         // Update
+        Vector2 oldPosition;
+        bool collision;
+        float dx = 0.0f;
+        float dy = 0.0f;
         switch (gameState) {
             case WAITING:
                 if (IsKeyPressed(KEY_ENTER) || IsKeyPressed(KEY_SPACE)) {
@@ -125,21 +94,35 @@ int main() {
                     break;
                 }
 
+
                 if (IsKeyDown(KEY_D) || IsKeyDown(KEY_RIGHT)) {
-                    player.setX(player.getX() + movementSpeed * deltaTime);
+                    dx += movementSpeed * deltaTime;
                 }
                 if (IsKeyDown(KEY_A) || IsKeyDown(KEY_LEFT)) {
-                    player.setX(player.getX() - movementSpeed * deltaTime);
+                    dx -= movementSpeed * deltaTime;
                 }
                 if (IsKeyDown(KEY_S) || IsKeyDown(KEY_DOWN)) {
-                    player.setY(player.getY() + movementSpeed * deltaTime);
+                    dy += movementSpeed * deltaTime;
                 }
                 if (IsKeyDown(KEY_W) || IsKeyDown(KEY_UP)) {
-                    player.setY(player.getY() - movementSpeed * deltaTime);
+                    dy -= movementSpeed * deltaTime;
                 }
                 if (IsKeyPressed(KEY_E)) {
                     timer = 10;
                 }
+
+                oldPosition = {player.getX(), player.getY()};
+
+                player.setX(player.getX() + dx);
+                if (CheckCollisionWithTexture(player.getHitbox(), wallPixels, officeWallsImage.width, officeWallsImage.height)) {
+                    player.setX(oldPosition.x);
+                }
+
+                player.setY(player.getY() + dy);
+                if (CheckCollisionWithTexture(player.getHitbox(), wallPixels, officeWallsImage.width, officeWallsImage.height)) {
+                    player.setY(oldPosition.y);
+                }
+
                 break;
             case FINISHED:
                 if (IsKeyPressed(KEY_ENTER) || IsKeyPressed(KEY_SPACE)) {
@@ -150,10 +133,6 @@ int main() {
                     gameState = PLAYING;
                 }
                 break;
-        }
-
-        if(CheckCollisionRecs(r1, player.getHitbox())) {
-            player.setX(player.getX() + movementSpeed * deltaTime);
         }
 
         camera.offset = (Vector2){GetScreenWidth() / 2.0f, GetScreenHeight() / 2.0f};
@@ -167,7 +146,8 @@ int main() {
         ClearBackground(BLACK);
         BeginMode2D(camera);
 
-        map.draw();
+        floor.draw();
+        walls.draw();
 
         DrawCircle(100, 75, 12.5, RED);
         if (CheckCollisionCircleRec((Vector2){100, 75}, 12.5, player.getHitbox())) {
@@ -176,62 +156,7 @@ int main() {
 
         player.draw();
 
-        if(IsKeyDown(KEY_F4)) {
-            DrawRectangleRec(t1, RED);
-            DrawRectangleRec(t2, RED);
-            DrawRectangleRec(t3, RED);
-            DrawRectangleRec(t4, RED);
-            DrawRectangleRec(t5, RED);
-            DrawRectangleRec(t6, RED);
-            DrawRectangleRec(t7, RED);
-            DrawRectangleRec(t8, RED);
-            DrawRectangleRec(t9, RED);
-            DrawRectangleRec(t10, RED);
-            DrawRectangleRec(t11, RED);
-            DrawRectangleRec(t12, RED);
-            DrawRectangleRec(t13, RED);
-
-            DrawRectangleRec(b1, BLUE);
-            DrawRectangleRec(b2, BLUE);
-            DrawRectangleRec(b3, BLUE);
-            DrawRectangleRec(b4, BLUE);
-            DrawRectangleRec(b5, BLUE);
-            DrawRectangleRec(b6, BLUE);
-            DrawRectangleRec(b7, BLUE);
-            DrawRectangleRec(b8, BLUE);
-            DrawRectangleRec(b9, BLUE);
-            DrawRectangleRec(b10, BLUE);
-
-            DrawRectangleRec(l1, YELLOW);
-            DrawRectangleRec(l2, YELLOW);
-            DrawRectangleRec(l3, YELLOW);
-            DrawRectangleRec(l4, YELLOW);
-            DrawRectangleRec(l5, YELLOW);
-            DrawRectangleRec(l6, YELLOW);
-            DrawRectangleRec(l7, YELLOW);
-            DrawRectangleRec(l8, YELLOW);
-            DrawRectangleRec(l9, YELLOW);
-            DrawRectangleRec(l10, YELLOW);
-            DrawRectangleRec(l11, YELLOW);
-            DrawRectangleRec(l12, YELLOW);
-
-            DrawRectangleRec(r1, GREEN);
-            DrawRectangleRec(r2, GREEN);
-            DrawRectangleRec(r3, GREEN);
-            DrawRectangleRec(r4, GREEN);
-            DrawRectangleRec(r5, GREEN);
-            DrawRectangleRec(r6, GREEN);
-            DrawRectangleRec(r7, GREEN);
-            DrawRectangleRec(r8, GREEN);
-            DrawRectangleRec(r9, GREEN);
-            DrawRectangleRec(r10, GREEN);
-        }
-
-
-
         EndMode2D();
-
-
 
         std::ostringstream stream;
         switch (gameState) {
@@ -255,13 +180,14 @@ int main() {
             DrawFPS(0, 32);
             DrawText(("Position: " + std::to_string(player.getX()) + "," + std::to_string(player.getY())).c_str(), 0, 48, 20, WHITE);
             DrawText(("DeltaTime: " + std::to_string(deltaTime)).c_str(), 0, 64, 20, WHITE);
-
-
         }
 
         EndDrawing();
     }
 
+    UnloadImageColors(wallPixels);
+    UnloadImage(officeWallsImage);
+    UnloadTexture(officeWalls);
     UnloadRenderTexture(target);
     CloseWindow();
 
